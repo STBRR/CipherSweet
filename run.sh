d361c788d942e365697541dd282c0a705e27456e
Liam <liam@gmx.us>
2022-05-11 21:47:19 +0100

    oops: accidentally pushed old version of file

--- run.sh
+++ run.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-cd ~/Tools/testssl.sh/
+# Define location for 'testssl' install
+TESTSSL_INSTALL_LOCATION="../testssl.sh"
+CIPHERSWEET_INSTALL="/Users/liam//Tools/ciphersweet/checker.py"
 
-echo "-=[ CipherSuite Testing Tool ]=-"
-#read -p '[!] URL: ' targetURL
+cd $TESTSSL_INSTALL_LOCATION
 
-targetURL=$1
-fullURL="$targetURL:443"
+# First argument will be the URL that is passed during a for loop / xargs etc..
+USER_TARGET=$1
+OUTPUT_FILENAME="/tmp/cipher-suite.txt"
+CIPHER_FILENAME="/tmp/cipher-suite-ciphers.txt"
 
-echo "[!] Running TestSSL against: $targetURL"
-testssl -P $fullURL > /tmp/cipher-suite.txt
+echo "[*] Running against: $USER_TARGET (This might take around 20-25 seconds)"
+testssl -P $USER_TARGET > $OUTPUT_FILENAME
 
 echo "[*] Parsing out Identified Ciphers"
-cat /tmp/cipher-suite.txt | grep 'TLS_' | awk '{print $7}' | grep 'TLS_' > /tmp/cipher-suite-ciphers.txt
+cat $OUTPUT_FILENAME | grep 'TLS_' | awk '{print $7}' > $CIPHER_FILENAME
 
+# Now that we've parsed out the ciphers into their own file, let's run the python script
 echo "[*] Checking Ciphers against CipherSuite API"
-cd ~/Tools/ciphersweet/
-for x in $(cat /tmp/cipher-suite-ciphers.txt); do python3 cipher_check.py $x; done
 
-echo "Cleaning up generated files."
-rm /tmp/cipher-suite.txt /tmp/cipher-suite-ciphers.txt
-echo "Thank for using"
+for cipher in $(cat $CIPHER_FILENAME);
+do
+	python3 $CIPHERSWEET_INSTALL $cipher
+done
 
+# Now that has done, let's cleanup the files that were created.
+rm $OUTPUT_FILENAME
+rm $CIPHER_FILENAME
